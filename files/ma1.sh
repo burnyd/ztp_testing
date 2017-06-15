c63ada7c546a1e214461e9ce38e4d1c0b057c488
burnyd <burnyd@gmail.com>
2017-06-15 14:12:15 -0400

    after adding ma0

--- files/ma1.sh
+++ files/ma1.sh
@@ -2,23 +2,17 @@
 
 MGT_IPADDRESS=$(ip addr list ma1 | grep inet |cut -d ' ' -f6|cut -d/ -f1)
 DEFAULT_GW=$(/sbin/ip route | awk '/default/ { print $3 }')
+CIDR=/24
 
-echo $MGT_IPADDRESS
-echo $DEFAULT_GW
-
+echo $MGT_IPADDRESS$CIDR
 
 FastCli -p 15 -c "configure 
-ip routing
-!
-ip routing vrf management
-!
 vrf definition management
    rd 100:100
 !
 interface Management1
-  vrf forwarding management
-  ip address $mgt_ipaddress/24
+  no ip address
+  ip address $MGT_IPADDRESS$CIDR
   no shutdown
 !
-ip route vrf management 0.0.0.0/0 $DEFAULT_GW
 exit"
